79bc853ebbd10a60cb15fc96e4d57025a2e0ba53
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2023-12-04 13:01:15 +0100

    Add std-like API for Mutex and CondVar

--- absl/synchronization/mutex.h
+++ absl/synchronization/mutex.h
@@ -1087,6 +1087,11 @@ class CondVar {
   // Note: this method substantially reduces `CondVar` performance.
   void EnableDebugLog(const char* absl_nullable name);
 
+  // std-like API
+  void notify_one() { Signal(); }
+
+  void notify_all() { SignalAll(); }
+
  private:
   bool WaitCommon(Mutex* absl_nonnull mutex,
                   synchronization_internal::KernelTimeout t);
