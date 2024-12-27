fc7cfa87b7b1a5b4dc88359d1224e0a13758fd9c
mbkkt <mbkkt@users.noreply.github.com>
2024-12-27 18:22:00 +0000

    Disable moved from

--- absl/container/internal/raw_hash_set.h
+++ absl/container/internal/raw_hash_set.h
@@ -3529,7 +3529,7 @@ class raw_hash_set {
     if (SwisstableGenerationsEnabled()) {
       that.common().set_capacity(this == &that
                                      ? HashtableCapacity::CreateSelfMovedFrom()
-                                     : HashtableCapacity::CreateMovedFrom());
+                                     : DefaultCapacity());
     }
     if (!SwisstableGenerationsEnabled() ||
         !maybe_invalid_capacity().IsValid() ||
