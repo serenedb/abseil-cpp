ed245a995096c76c0e32458853a20c5f6c7115ce
mbkkt <mbkkt@users.noreply.github.com>
2026-04-24 09:57:24 +0200

    Fix compile for move out

--- absl/container/internal/raw_hash_set.h
+++ absl/container/internal/raw_hash_set.h
@@ -1355,9 +1355,7 @@ class CommonFields : public CommonFieldsGenerationInfo {
     return inline_data_.capacity();
   }
   void set_capacity(HashtableCapacity c) { inline_data_.set_capacity(c); }
-  void set_capacity(size_t c) {
-    set_capacity(HashtableCapacity(c));
-  }
+  void set_capacity(size_t c) { set_capacity(HashtableCapacity(c)); }
   bool is_small() const { return inline_data_.is_small(); }
 
   GrowthInfoAccessor growth_info() const {
@@ -2330,7 +2328,7 @@ class raw_hash_set {
     using value_type = typename raw_hash_set::value_type;
     using reference =
         std::conditional_t<PolicyTraits::constant_iterators::value,
-                            const value_type&, value_type&>;
+                           const value_type&, value_type&>;
     using pointer = std::remove_reference_t<reference>*;
     using difference_type = typename raw_hash_set::difference_type;
 
@@ -3073,8 +3071,7 @@ class raw_hash_set {
 
     if (src.is_small()) {
       if (src.empty()) return;
-      if (insert_slot(src.single_slot()))
-        src.erase_meta_only_small();
+      if (insert_slot(src.single_slot())) src.erase_meta_only_small();
       return;
     }
     for (auto it = src.begin(), e = src.end(); it != e;) {
@@ -3166,8 +3163,7 @@ class raw_hash_set {
 
   template <class K = key_type>
   ABSL_DEPRECATE_AND_INLINE()
-  iterator find(const key_arg<K>& key,
-                size_t) ABSL_ATTRIBUTE_LIFETIME_BOUND {
+  iterator find(const key_arg<K>& key, size_t) ABSL_ATTRIBUTE_LIFETIME_BOUND {
     return find(key);
   }
   // The API of find() has one extension: the type of the key argument doesn't
@@ -3182,8 +3178,8 @@ class raw_hash_set {
 
   template <class K = key_type>
   ABSL_DEPRECATE_AND_INLINE()
-  const_iterator find(const key_arg<K>& key,
-                      size_t) const ABSL_ATTRIBUTE_LIFETIME_BOUND {
+  const_iterator
+      find(const key_arg<K>& key, size_t) const ABSL_ATTRIBUTE_LIFETIME_BOUND {
     return find(key);
   }
   template <class K = key_type>
@@ -3512,7 +3508,7 @@ class raw_hash_set {
     if (SwisstableGenerationsEnabled()) {
       that.common().set_capacity(this == &that
                                      ? HashtableCapacity::CreateSelfMovedFrom()
-                                     : DefaultCapacity());
+                                     : HashtableCapacity{DefaultCapacity()});
     }
     if (!SwisstableGenerationsEnabled() ||
         !maybe_invalid_capacity().IsValid() ||
