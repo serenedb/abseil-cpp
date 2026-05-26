4075cfe7b11651320e1b25e783d9f17d0386149a
mbkkt <mbkkt@users.noreply.github.com>
2026-05-26 15:00:05 +0000

    Refactor AssertHashEqConsistent to use constexpr for type checks and improve readability

--- absl/container/internal/raw_hash_set.h
+++ absl/container/internal/raw_hash_set.h
@@ -3598,17 +3598,17 @@ class raw_hash_set {
   // meaning that `eq(k1, k2)` implies `hash(k1)==hash(k2)`.
   template <class K>
   void AssertHashEqConsistent(const K& key) {
-#ifdef NDEBUG
-    return;
-#endif
+#ifndef NDEBUG
     // If the hash/eq functors are known to be consistent, then skip validation.
-    if (std::is_same<hasher, absl::container_internal::StringHash>::value &&
-        std::is_same<key_equal, absl::container_internal::StringEq>::value) {
+    if constexpr (std::is_same<hasher,
+                               absl::container_internal::StringHash>::value &&
+                  std::is_same<key_equal,
+                               absl::container_internal::StringEq>::value) {
       return;
     }
-    if (std::is_scalar<key_type>::value &&
-        std::is_same<hasher, absl::Hash<key_type>>::value &&
-        std::is_same<key_equal, std::equal_to<key_type>>::value) {
+    if constexpr (std::is_scalar<key_type>::value &&
+                  std::is_same<hasher, absl::Hash<key_type>>::value &&
+                  std::is_same<key_equal, std::equal_to<key_type>>::value) {
       return;
     }
     if (empty()) return;
@@ -3629,9 +3629,18 @@ class raw_hash_set {
       assert_consistent(/*unused*/ nullptr, single_slot());
       return;
     }
+    if constexpr (requires { typename hasher::skip_absl_hash_eq_consistent; }) {
+      return;
+    }
+    if constexpr (requires {
+                    typename key_equal::skip_absl_hash_eq_consistent;
+                  }) {
+      return;
+    }
     // We only do validation for small tables so that it's constant time.
     if (capacity() > 16) return;
     IterateOverFullSlots(common(), sizeof(slot_type), assert_consistent);
+#endif
   }
 
   // Attempts to find `key` in the table; if it isn't found, returns an iterator
